edb1bb6ee4f3a484824d058135609b26f1c574c2
bhanuprakashh <59020797+bhanuprakashh@users.noreply.github.com>
2019-12-25 16:44:17 +0530

    Add files via upload

---
+++ 23.py
@@ -0,0 +1,14 @@
+a=int(input("enter a value"))
+b=int(input("enter b value"))
+c=a+b
+d=a-b
+e=a*b
+print(c)
+print(d)
+print(e)
+try:
+    f=a/b
+    print(f)
+except:
+    print("zero dilvisipn")
+
